e1bebc64025e3b086fab275b7043e7fc1a81a050
Kozar <108079767+Kozar0001@users.noreply.github.com>
2022-11-07 15:31:02 -0500

    Add files via upload

---
+++ 1st-python-project-BlockLetters.py
@@ -0,0 +1,16 @@
+#Block Letters Leggo
+#Ben Kozar
+#I am a collectoooor
+print ('BBBB     K   K     ')
+print ('B   B    K  K      ')
+print ('B   B    K K       ')
+print ('BBBB     KK        ')
+print ('B   B    K K       ')
+print ('B   B    K  K      ')
+print ('BBBB     K   K     ')
+
+
+
+
+
+
