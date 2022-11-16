703193aeaa48c2a37fd57479ef60fd9048726882
Kozar <108079767+Kozar0001@users.noreply.github.com>
2022-11-16 13:41:05 -0500

    Add files via upload

---
+++ Shipping.py
@@ -0,0 +1,28 @@
+weight = 5
+
+# Ground Shipping
+if weight <= 2:
+  cost_ground = weight * 1.5 + 20
+elif weight <= 6:
+  cost_ground = weight * 3 + 20
+elif weight <= 10:
+  cost_ground = weight * 4 + 20
+else:
+  cost_ground = weight * 4.75 + 20
+print(cost_ground)
+
+#Premium Shipping
+ground_shipping_premium = 125.00
+print(ground_shipping_premium)
+
+#Drone Shipping
+if weight <= 2:
+  cost_drone = weight * 4.50
+elif weight <= 6:
+  cost_drone = weight * 9.00
+elif weight <= 10:
+  cost_drone = weight * 12.00
+else:
+  cost_drone = weight * 14.25
+print(cost_drone)
+
